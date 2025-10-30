b9390f9d04a76a5d44136189e8d7a9420cfbc131
Shuja-2406 <240541608@firat.edu.tr>
2025-10-30 10:32:13 +0300

    Update OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,7 +1,7 @@
-/*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ /*
+ * Ad Soyad: [Shuja Ahmad Tariq]
+ * Ogrenci No: [240541608]
+ * Tarih: [30-10-2025]
  * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
  * 
  * Bu program kullanicidan ogrenci bilgilerini alir ve
@@ -13,48 +13,61 @@ import java.util.Scanner;
 
 public class OgrenciBilgi {
     public static void main(String[] args) {
-        // Scanner objesi olusturun
-        
-        
-        // Degisken tanimlamalari
-        // String ad, soyad;
-        // int ogrenciNo, yas;
-        // double gpa;
-        
-        
-        // Kullanicidan bilgileri alin
+            // Sabit tanımlama
+        final double MAX_GPA = 4.00;
+
+        // Scanner nesnesi oluşturma
+        Scanner input = new Scanner(System.in);
+
+        // Kullanıcıdan bilgileri alma
+        System.out.print("Adinizi giriniz: ");
+        String ad = input.next();
+
+        System.out.print("Soyadinizi giriniz: ");
+        String soyad = input.next();
+
+        System.out.print("ogrenci numaranizi giriniz: ");
+        int ogrenciNo = input.nextInt();
+
+        System.out.print("Yasinizi giriniz: ");
+        int yas = input.nextInt();
+
+        System.out.print("Not ortalamanizi (0.00 - 4.00) giriniz: ");
+        double gpa = input.nextDouble();
         System.out.println("=== OGRENCI BILGI SISTEMI ===");
         System.out.println();
         
         // Ad
-        
+        Shuja 
         
         // Soyad
-        
+        Tariq
         
         // Ogrenci No
-        
+        240541608
         
         // Yas
-        
+        20
         
         // GPA
-        
+        3.16
         
         // Bilgileri ekrana yazdirin
         System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
         
         // COZUMUNUZU BURAYA YAZIN
+                System.out.println("\n--- OGRENCI BILGILERI ---");
+        System.out.printf("Ad Soyad        : %s %s%n", ad, soyad);
+        System.out.printf("Ogrenci No      : %d%n", ogrenciNo);
+        System.out.printf("Yas             : %d%n", yas);
+        System.out.printf("GPA             : %.2f / %.2f%n", gpa, MAX_GPA);
         
         
-        
-        
-        
-        
-
         
         
         // Scanner'i kapatin (önemli pratik)
+         // Scanner kapatma
+        input.close();
         
     }
 }
